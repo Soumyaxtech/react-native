e83678aabe7e41a134c66495cb9e11a0037fcd8e
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2023-09-10 17:39:02 +0530

    do it

--- javascript/hello.js
+++ javascript/hello.js
@@ -1,5 +1,10 @@
-function addthreenumber(a,b,c){
-    return a+b+c;
+// function addthreenumber(a,b,c){
+//     return a+b+c;
+// }
+// let c = addthreenumber(2,7,123)
+// console.log(c)
+function subthreenumber(a,b,c){
+        return a-b-c;
 }
-let c = addthreenumber(2,7,123)
-console.log(c)
+let c= subthreenumber(34,7,12)
+    console.log(c)
